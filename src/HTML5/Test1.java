e077308c20fe029bcef4f87c8325e1353c11f352
pdj <pdj1018@gmail.com>
2020-04-23 10:05:17 +0900

    Practice 04/23

---
+++ src/HTML5/Test1.java
@@ -0,0 +1,17 @@
+package HTML5;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// client	-	 internet 	- 	web server 
+		//Client Side
+		// - HTML5
+		// - CSS5
+		// -javascript
+		
+		//Server Side
+		// - jsp, servlet
+		// - database
+	}
+
+}
